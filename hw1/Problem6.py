828ed250f21b1997b38f472496534e53869ecb12
yuan <sjtuliuyuan@gmail.com>
2017-09-22 22:51:17 -0400

    finish problem 6

--- hw1/Problem6.py
+++ hw1/Problem6.py
@@ -106,17 +106,6 @@ def plot_loss_average(info = 'cross_entropy_loss', ymax = 1, NLL=True):
     plt.legend()
     return fig
 
-# Problem a
-'''
-fig = plot_loss_average(info = 'cross_entropy_loss', ymax = 1, NLL=True)
-fig.savefig('problem_a.png')
-'''
-# Problem b
-'''
-fig = plot_loss_average(info = 'incorrect classification ratio', ymax = 0.5, NLL=False)
-fig.savefig('problem_b.png')
-'''
-
 '''
     Problem c
 '''
@@ -134,12 +123,6 @@ def plot_visulizing_parameter(samples, num):
         plt.imshow(sample.reshape(28, 28), cmap='Greys_r')
 
     return fig
-# Problem c
-'''
-_, _, model = get_loss_one_time(NLL=True)
-fig = plot_visulizing_parameter(np.transpose(model.layer1.W[:-1,:]), 10)
-fig.savefig('problem_c.png')
-'''
 
 '''
     Problem d
@@ -188,20 +171,6 @@ def plot_problem_d_momentum(info, ymax, NLL):
     plt.legend()
     return fig
 
-# Problem d
-'''
-fig = plot_problem_d_learning_rt('cross_entropy_loss', 2, NLL=True)
-fig.savefig('problem_d_learning_rt_cross_entropy.png')
-
-fig = plot_problem_d_learning_rt('incorrect classification ratio', 1, NLL=False)
-fig.savefig('problem_d_learning_rt_IC.png')
-
-
-fig = plot_problem_d_momentum('cross_entropy_loss', 3, NLL=True)
-fig.savefig('problem_d_momentum_cross_entropy.png')
-fig = plot_problem_d_momentum('incorrect classification ratio', 1, NLL=False)
-fig.savefig('problem_d_momentum_IC.png')
-'''
 
 '''
     Problem e
@@ -244,12 +213,6 @@ def plot_problem_e():
     p2.grid(True)
     p2.legend()
     return fig
-
-# Problem e
-'''
-fig = plot_problem_e()
-fig.savefig('problem_e.png')
-'''
 
 # Problem f
 def plot_problem_f_l2_reg():
@@ -295,54 +258,6 @@ def plot_problem_f_or_g_result(model, epoch_num, learning_rt, momentum, alpha):
     return fig
 
 '''
-# Problem f
-fig = plot_problem_f_l2_reg()
-fig.savefig('problem_f_find_l2.png')
-#train_loss_NLL 0.0302798907306 valid_loss_NLL 0.274775637271 test_loss_NLL 0.325125106534
-#train_loss_IC 0.0 valid_loss_IC 0.084 test_loss_IC 0.0923333333333
-model = NN_3_layer()
-fig = plot_problem_f_or_g_result(model, 180, 0.01, 0, 0.001)
-fig.savefig('problem_f_visualization.png')
-'''
-
-'''
-    Problem g
-'''
-'''
-model = NN_4_layer()
-#train_loss_NLL 0.324571271092 valid_loss_NLL 0.499887242168 test_loss_NLL 0.552144295303
-#train_loss_IC 0.101 valid_loss_IC 0.149 test_loss_IC 0.166666666667
-fig = plot_problem_f_or_g_result(model, 140, 0.01, 0, .0)
-fig.savefig('problem_g_visualization.png')
-'''
-
-'''
-    Problem h
-'''
-'''
-model = NN_4_BN_layer()
-#Epoch num: 7
-#train_loss_NLL 0.115590739458 valid_loss_NLL 0.359999605125 test_loss_NLL 0.528242543282
-#train_loss_IC 0.036 valid_loss_IC 0.118 test_loss_IC 0.138
-for i in range(30):
-
-    print 'Epoch num:', i
-    # Train the model
-    train_or_evaluate_epoch(model, train_data, Train = True,
-                         learning_rt = 0.1, momentum = 0, NLL = True, alpha = 0)
-
-    train_loss_NLL = train_or_evaluate_epoch(model, train_data, Train = False, NLL = True)
-    valid_loss_NLL = train_or_evaluate_epoch(model, valid_data, Train = False, NLL = True)
-    test_loss_NLL= train_or_evaluate_epoch(model, test_data, Train = False, NLL = True)
-    train_loss_IC = train_or_evaluate_epoch(model, train_data, Train = False, NLL = False)
-    valid_loss_IC = train_or_evaluate_epoch(model, valid_data, Train = False, NLL = False)
-    test_loss_IC= train_or_evaluate_epoch(model, test_data, Train = False, NLL = False)
-    print 'train_loss_NLL', train_loss_NLL, 'valid_loss_NLL', valid_loss_NLL, 'test_loss_NLL', test_loss_NLL
-    print 'train_loss_IC', train_loss_IC, 'valid_loss_IC', valid_loss_IC, 'test_loss_IC', test_loss_IC
-'''
-
-
-'''
     Problem i
 '''
 def plot_problem_i():
@@ -380,8 +295,3 @@ def plot_problem_i():
     p2.grid(True)
     p2.legend()
     return fig
-'''
-# Problem i
-fig = plot_problem_i()
-fig.savefig('problem_i.png')
-'''
---
+++ hw1/Readme.txt
@@ -0,0 +1,7 @@
+run the code by following command:
+python main.py --problem a
+python main.py --problem b
+.
+.
+.
+python main.py --problem i
